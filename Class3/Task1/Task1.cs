b70a133a38ca7e1fc925b872fe50424280f7906a
Андриянов Александр <alexandri2003@gmail.com>
2022-09-18 11:35:26 +0300

    task1 completed

--- Class3/Task1/Task1.cs
+++ Class3/Task1/Task1.cs
@@ -1,4 +1,6 @@
 ﻿// Колода
+
+using Console = System.Console;
 using Deck = System.Collections.Generic.List<Card>;
 // Набор карт у игрока
 using Hand = System.Collections.Generic.List<Card>;
@@ -6,21 +8,36 @@ using Hand = System.Collections.Generic.List<Card>;
 using Table = System.Collections.Generic.List<Card>;
 
 // Масть
-internal enum Suit
+internal enum Suit : ushort
 {
+    Hearts = 4,
+    Diamonds = 3,
+    Spades = 2,
+    Clubs = 1
 }
 
 // Значение
-internal enum Rank
+internal enum Rank : ushort
 {
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13,
+    Ace = 14
 }
 
 // Карта
-record Card;
+record Card(Suit CardSuit, Rank CardRank);
 
 // Тип для обозначения игрока (первый, второй)
 internal enum Player
 {
+    First,
+    Second
 }
 
 namespace Task1
@@ -44,31 +61,133 @@ namespace Task1
         // Возвращается null, если значения карт совпадают
         internal static Player? RoundWinner(Card card1, Card card2)
         {
-            throw new NotImplementedException();
+            if (card1.CardRank > card2.CardRank)
+            {
+                return Player.First;
+            }
+
+            if (card2.CardRank > card1.CardRank)
+            {
+                return Player.Second;
+            }
+
+            return null;
         }
         
 // Возвращает полную колоду (36 карт) в фиксированном порядке
-        internal static Deck FullDeck() 
+        internal static Deck FullDeck()
         {
-            throw new NotImplementedException();
+            Deck deck = new Deck();
+
+            foreach (Suit iSuit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank jRank in Enum.GetValues(typeof(Rank)))
+                {
+                    deck.Add(new Card(iSuit, jRank));
+                }
+            }
+
+            return deck;
         }
 
 // Раздача карт: случайное перемешивание (shuffle) и деление колоды пополам
-        internal static Dictionary<Player, Hand> Deal(Deck deck) 
+        internal static Dictionary<Player, Hand> Deal(Deck deck)
         {
-            throw new NotImplementedException();
+            Deck shuffledDeck = deck;
+            Random random = new Random();
+
+            for (int i = 0; i < DeckSize; i++)
+            {
+                int swapWithInd = random.Next(DeckSize - 1);
+                (shuffledDeck[i], shuffledDeck[swapWithInd]) = (shuffledDeck[swapWithInd], shuffledDeck[i]);
+            }
+
+            Dictionary<Player, Hand> gameSet = new Dictionary<Player, Deck>
+            {
+                { Player.First, shuffledDeck.GetRange(0, 18) },
+                { Player.Second, shuffledDeck.GetRange(18, 18) }
+            };
+
+            return gameSet;
         }
 
 // Один раунд игры (в том числе спор при равных картах).
 // Возвращается победитель раунда и набор карт, выложенных на стол.
-        internal static Tuple<Player, Table> Round(Dictionary<Player, Hand> hands) {
-            throw new NotImplementedException();
+        internal static Tuple<Player, Table> Round(Dictionary<Player, Hand> hands)
+        {
+            Hand firstPlayersHand = hands[Player.First];
+            Hand secondPlayersHand = hands[Player.Second];
+            Player? roundWinner = null;
+            Table table = new Table();
+
+            do
+            {
+                roundWinner = RoundWinner(firstPlayersHand.First(), secondPlayersHand.First());
+                table.Add(firstPlayersHand.First());
+                firstPlayersHand.RemoveAt(0);
+                table.Add(secondPlayersHand.First());
+                secondPlayersHand.RemoveAt(0);
+
+            } while (firstPlayersHand.Count > 0 && secondPlayersHand.Count > 0 && roundWinner == null);
+
+            var winner = roundWinner.GetValueOrDefault(table[^2].CardSuit > table[^1].CardSuit ? Player.First : Player.Second);
+
+            Tuple<Player, Table> result = new Tuple<Player, Table>(winner, table);
+
+            return result;
         }
 
 // Полный цикл игры (возвращается победивший игрок)
 // в процессе игры печатаются ходы
-        internal static Player Game(Dictionary<Player, Hand> hands) {
-            throw new NotImplementedException();
+        internal static Player Game(Dictionary<Player, Hand> hands)
+        {
+            Hand firstPayersHand = hands[Player.First];
+            Hand secondPayersHand = hands[Player.Second];
+            Tuple<Player, Table> round = new Tuple<Player, Deck>(Player.First, new Table());
+            int roundsCounter = 1;
+
+            while (firstPayersHand.Any() && secondPayersHand.Any() && roundsCounter <= 100)
+            {
+                Console.WriteLine($"Round {roundsCounter++}");
+
+                var current = new Dictionary<Player, Hand>
+                {
+                    { Player.First, firstPayersHand }, { Player.Second, secondPayersHand }
+                };
+                round = Round(current);
+
+                int pairChecker = 1;
+                foreach (var card in round.Item2)
+                {
+                    Console.Write($"{card.CardRank} of {card.CardSuit} ");
+                    
+                    if (firstPayersHand.Contains(card)) firstPayersHand.Remove(card);
+                    if (secondPayersHand.Contains(card)) secondPayersHand.Remove(card);
+
+                    if (pairChecker == 1)
+                    {
+                        Console.Write("VS ");
+                        pairChecker++;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        pairChecker--;
+                    }
+                }
+                
+                Console.WriteLine($"{round.Item1} wins the round\n");
+
+                if (round.Item1 == Player.First)
+                    foreach (var card in round.Item2)
+                        firstPayersHand.Add(card);
+
+                else
+                    foreach (var card in round.Item2)
+                        secondPayersHand.Add(card);
+            }
+
+            return round.Item1;
         }
 
         public static void Main(string[] args)
--- Class3/Task1/Task1Test.cs
+++ Class3/Task1/Task1Test.cs
@@ -9,7 +9,9 @@ public class Tests
     [Test]
     public void RoundWinnerTest()
     {
-        throw new NotImplementedException();
+        That(Equals(RoundWinner(new Card(Suit.Diamonds, Rank.Six), new Card(Suit.Hearts, Rank.Queen)), Player.Second));
+        That(Equals(RoundWinner(new Card(Suit.Spades, Rank.Ten), new Card(Suit.Clubs, Rank.Seven)), Player.First));
+        That(Equals(RoundWinner(new Card(Suit.Hearts, Rank.Ace), new Card(Suit.Clubs, Rank.Ace)), null));
     }
 
     [Test]
@@ -17,31 +19,53 @@ public class Tests
     {
         var deck = FullDeck();
         That(deck, Has.Count.EqualTo(DeckSize));
-        throw new NotImplementedException();
+        
+        foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+        {
+            That(deck.FindAll(card => card.CardRank == rank), Has.Count.EqualTo(4));
+        }
+        
+        foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+        {
+            That(deck.FindAll(card => card.CardSuit == suit), Has.Count.EqualTo(9));
+        }
     }
 
     [Test]
     public void RoundTest()
     {
-        throw new NotImplementedException();
+        Dictionary<Player, List<Card>> hands = new Dictionary<Player, List<Card>>
+        {
+            { Player.First, new List<Card> { new Card(Suit.Clubs, Rank.Ace), new Card(Suit.Diamonds, Rank.Eight) } },
+            { Player.Second, new List<Card> { new Card(Suit.Hearts, Rank.Ace), new Card(Suit.Spades, Rank.Ten) } }
+        };
+        
+        Tuple<Player, List<Card>> result = new Tuple<Player, List<Card>>(Player.Second, new List<Card>
+        {
+            new Card(Suit.Clubs, Rank.Ace),new Card(Suit.Hearts, Rank.Ace),
+            new Card(Suit.Diamonds, Rank.Eight), new Card(Suit.Spades, Rank.Ten),
+        });
+
+        var test = Round(hands);
+        That(test is { Item1: Player.Second });
+        That(test != null && test.Item2.Count() == 4);
+        for (int i = 0; i < 4; i++)
+        {
+            That(test != null && test.Item2[i] == result.Item2[i]);
+        }
     }
 
     [Test]
     public void Game2CardsTest()
     {
-        var six = TODO<Card>();
-        var ace = TODO<Card>();
+        var six = new Card(Suit.Diamonds,Rank.Six);
+        var ace = new Card(Suit.Hearts, Rank.Ace);
         Dictionary<Player, List<Card>> hands = new Dictionary<Player, List<Card>>
         {
-            { TODO<Player>(), new List<Card> {six} },
-            { TODO<Player>(), new List<Card> {ace} }
+            { Player.First, new List<Card> {new Card(Suit.Diamonds,Rank.Six)} },
+            { Player.Second, new List<Card> {new Card(Suit.Hearts, Rank.Ace)} }
         };
         var gameWinner = Game(hands);
-        That(gameWinner, Is.EqualTo(TODO<Player>()));
-    }
-    
-    private static T TODO<T>()
-    {
-        throw new NotImplementedException();
+        That(gameWinner, Is.EqualTo(Player.Second));
     }
 }
